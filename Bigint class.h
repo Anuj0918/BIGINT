446e8821863a306964fdbd3d4f01079606334d6e
Anuj <anujtiwari2822@gmail.com>
2024-04-05 20:21:14 +0530

    Implement

--- Bigint class.h
+++ Bigint class.h
@@ -22,7 +22,7 @@ using namespace std;
 #define sortBig BigInteger::sortBigInteger
 #define powBig BigInteger::powBigInteger
 
-class BigInteger {
+class BigInteger { 
 
     private:
         string str;
@@ -55,4 +55,243 @@ class BigInteger {
         convertstr << n;
         str = convertstr.str();
     }
+      BigInteger (const BigInteger &N2)
+    {
+        if(!isInputCorrect(N2.str)) {
+            cerr << "Invalid Input.. Please Use Valid BhimInteger" << endl;
+            exit(0);
+        }
+        str = N2.str;
+    }
+
+    // Input Validator
+    bool isInputCorrect(string s) {
+        if(s[0] == '-')
+            s.erase(0, 1);
+        for(int i = 0; i < s.length(); ++i) {
+            if((int)s[i] < 48 || (int)s[i] > 57)
+                return false;
+        }
+        return true;
+    }
+
+    friend ostream &operator<<( ostream &output, const BigInteger &N )
+    {
+         output<<N.str;
+         return output;
+    }
+
+    friend istream &operator>>( istream  &input, BigInteger &N )
+    {
+         input >> N.str;
+         return input;
+    }
+
+    static BigInteger to_BigInteger(string s)
+    {
+        BhimInteger N3;
+        N3.str = s;
+        return N3;
+    }
+
+    static BigInteger to_BigInteger(int n)
+    {
+        BhimInteger N3;
+        ostringstream convertstr;
+        convertstr << n;
+        N3.str = convertstr.str();
+        return N3;
+    }
+
+    static BigInteger to_BigInteger(long long int n)
+    {
+        BigInteger N3;
+        ostringstream convertstr;
+        convertstr << n;
+        N3.str = convertstr.str();
+        return N3;
+    }
+
+   static string removeZero(string str)
+    {
+        int i = 0, flag = 0;
+        if(str[0] == '-')
+        {
+            flag = 1;
+            i++;
+        }
+        while (str[i] == '0')
+           i++;
+        if(flag)
+            str.erase(1, i);
+        else
+            str.erase(0, i);
+        return str;
+    }
+
+    static string add(string s, string s1)
+    {
+        int carry=0,i,j;
+        int sum=0;
+        int m=s.length();
+        int n=s1.length();
+        string y = "";
+        if(m == 0 && n == 0)
+        {
+            y = "0";
+        }
+        else if(s[0] == '-' && s1[0] == '-')
+        {
+            y = add(s.erase(0,1), s1.erase(0, 1));
+            y = '-' + y;
+        }
+        else if(s[0] == '-')
+        {
+            y = subtract(s1, s.erase(0, 1));
+        }
+        else if(s1[0] == '-')
+        {
+            y = subtract(s, s1.erase(0, 1));
+        }
+        else
+        {
+            vector<int>v;
+            for(i=m-1,j=n-1;i>=0||j>=0;i--,j--)
+            {
+                sum=carry;
+                if(i>=0)
+                {
+                    sum+=s[i]-'0';
+                }
+                if(j>=0)
+                {
+                    sum+=s1[j]-'0';
+                }
+                v.push_back(sum%10);
+                carry=sum/10;
+            }
+            if(carry!=0)
+            v.push_back(carry);
+
+            for(i=v.size()-1;i>=0;i--)
+                y += (char)(v[i]+48);
+        }
+        return y;
+    }
+
+    static string subtract(string s1,string s2)
+    {
+        string answer="",ans,temp;
+        int carry=0,i,j,k,y=0,flag=0,flag1=0;
+        int sum=0;
+        if(s1==s2)
+        {
+            return "0";
+        }
+        else if(s1[0]=='-' and s2[0]=='-')
+        {
+            flag1=1;
+            s1=s1.substr(1);
+            s2=s2.substr(1);
+        }
+        else if(s1[0]=='-' and s2[0]!='-')
+        {
+            s1=s1.substr(1);
+            answer=add(s1,s2);
+            answer='-'+answer;
+            return answer;
+        }
+        else if(s1[0]!='-' and s2[0]=='-')
+        {
+            s2=s2.substr(1);
+            answer=add(s1,s2);
+            return answer;
+        }
+        ans=maxCompare(s1,s2);
+        if(ans==s2)
+        {
+            temp=s1;
+            s1=s2;
+            s2=temp;
+            flag=1;
+        }
+        int m=s1.length();
+        int n=s2.length();
+        if(m>n)
+        k=m;
+        else
+        k=n;
+        int A[k+1]={0};
+        for(i=m-1,j=n-1;i>=0||j>=0;i--,j--)
+        {
+            sum=0;
+            if(i>=0)
+            {
+                sum+=s1[i]-'0';
+            }
+            if(j>=0)
+            {
+                if(sum<s2[j]-'0'+carry)
+                {
+                    sum+=10;
+                    sum-=s2[j]-'0'+carry;
+                    carry=1;
+                }
+                else
+                {
+                    sum-=s2[j]-'0'+carry;
+                    carry=0;
+                }
+            }
+            if(carry!=0 and j<0)
+            {
+              if(sum<carry)
+              {
+                sum+=10;
+                sum-=carry;
+                carry=1;
+              }
+              else
+              {
+                sum-=carry;
+                carry=0;
+              }
+            }
+            A[y++]=sum;
+            //carry=sum/10;
+        }
+        for(i=k-1;i>=0;i--)
+        {
+            answer += (char)(A[i] + 48);
+        }
+        answer=removeZero(answer);
+        if(flag1==1 and flag==0)
+        {
+            answer='-'+answer;
+        }
+        else if(flag1==0 and flag==1)
+        {
+            answer='-'+answer;
+        }
+        return answer;
+    }
+
+    static string multiply(string s, string s1)
+    {
+        int flag=0;
+        int m=s.length();
+        int n=s1.length();
+        int A[m+n]={0};
+        for(int i=m-1;i>=0;i--)
+        {
+            for(int j=n-1;j>=0;j--)
+            {
+                int mul=(s[i]-'0')*(s1[j]-'0');
+                int sum=A[i+j+1]+mul;
+                A[i+j]+=sum/10;
+                A[i+j+1]=sum % 10;
+            }
+        }
+    
+}
 }
--- Samplecode.cpp
+++ Samplecode.cpp
@@ -4,5 +4,33 @@
 using namespace std;
 int main() 
 {
+      BigInteger n1;                                             //n1 defined with value 0
+    BigInteger n2(123);                                        //n2 defined with value int value
+    BigInteger n3((long long int)1234567898765432);            //n3 defined with value long long int value
+    BigInteger n4("7832467326423873423435");                   //n4 defined with string value
+    BigInteger n5(n3);                                         //n5 defined with values of n3
+
+    /********Converting to BhimNumbers*********/
+    int num1 = -321;
+    long long int num2 = -9876543219876543;
+    string str1 = "-2112321321321312421534365777";
+    BigInteger n6 = to_Big(num1);                              //converting int to BigInteger
+    BigInteger n7 = to_Big(num2);                              //converting long long int to BigInteger
+    BigInteger n8 = to_Big(str1);                              //converting string to BigInteger
+
+    /********user Input*********/
+    BigInteger n9;
+    cout<<"Enter the value for n9: ";
+    cin>>n9;                                                     //Taking input from user
+
+    cout<<"n1: "<<n1<<endl;                                      /**           Printing Values        **/
+    cout<<"n2: "<<n2<<endl;                                      /**           Printing Values        **/
+    cout<<"n3: "<<n3<<endl;                                      /**           Printing Values        **/
+    cout<<"n4: "<<n4<<endl;                                      /**           Printing Values        **/
+    cout<<"n5: "<<n5<<endl;                                      /**           Printing Values        **/
+    cout<<"n6: "<<n6<<endl;                                      /**           Printing Values        **/
+    cout<<"n7: "<<n7<<endl;                                      /**           Printing Values        **/
+    cout<<"n8: "<<n8<<endl;                                      /**           Printing Values        **/
+
     
 }
